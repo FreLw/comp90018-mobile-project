3be7294042a6eadabcaa79e408c1021bd2299ce5
Yunxiao Lu <lu.yunxiao@kinkotec.com>
2026-09-09 21:38:34 +1000

    Clear main branch

--- app/src/main/java/com/unimelb/losttreasures/ui/model/ChatMessage.kt
+++
@@ -1,7 +0,0 @@
-package com.unimelb.losttreasures.ui.model
-
-data class ChatMessage(
-    val author: String,
-    val text: String,
-    val mine: Boolean
-)
--- app/src/main/java/com/unimelb/losttreasures/ui/model/SocialPost.kt
+++
@@ -1,9 +0,0 @@
-package com.unimelb.losttreasures.ui.model
-
-data class SocialPost(
-    val author: String,
-    val location: String,
-    val message: String,
-    val timeAgo: String,
-    val likes: Int
-)
